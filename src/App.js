e8f8bfe1a7e24ee4889ca5d7dcdda21c125022ce
Chandana Kumara <pushpakumara.chandana@gmail.com>
2024-09-03 22:58:08 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
-        <Container>
-          <Button variant="primary">Character Count</Button>{' '}
-        </Container>
+    <div>
+      <Form.Group className="mb-3" controlId="sentenceTextArea">        
+        <Form.Control
+          as="textarea"
+          rows={3}
+          cols={4}
+          placeholder="Enter your text here"
+        />
+      </Form.Group>
+      
+      <Button variant="primary">Character Count</Button>
+ 
+    </div>
   );
 }
 
