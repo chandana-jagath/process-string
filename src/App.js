b1d4b0dff1d46dcfac2c0a986adf09b5b51f989d
Chandana Kumara <pushpakumara.chandana@gmail.com>
2024-09-03 21:54:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+        <Container>
+          <Button variant="primary">Character Count</Button>{' '}
+        </Container>
+  );
+}
+
+export default App;
